7600e7eb8886d58f23a86eb38b75a935f5743166
Thai Vu <thaivu220802@gmail.com>
2020-09-02 17:22:42 +0700

    ex5.py final

--- ex5.py
+++ ex5.py
@@ -9,7 +9,7 @@ my_hair = 'Brown'
 print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy.")
-print("Actually that's not too heavy.
+print("Actually that's not too heavy.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
